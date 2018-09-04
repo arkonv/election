4fb8cc4d2272984809d8820e527d686ad7ee7d10
Vik <superarkon@gmail.com>
2018-09-04 22:42:36 +0300

    Auto populate DB added

---
+++ src/main/java/ru/smartsarov/election/uik/UikWithMembers.java
@@ -0,0 +1,54 @@
+package ru.smartsarov.election.uik;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+
+import ru.smartsarov.election.db.Uik;
+import ru.smartsarov.election.db.UikMember;
+
+public class UikWithMembers extends Uik {
+	private static final long serialVersionUID = 1L;
+
+	@Expose
+	private List<UikMember> uikMembers;
+
+	@Expose
+	private /*BigDecimal*/String lat;
+	
+	@Expose
+	private /*BigDecimal*/String lng;
+
+	public UikWithMembers() {
+		super();
+		
+		uikMembers = new LinkedList<>();
+		
+	}
+
+	public List<UikMember> getUikMembers() {
+		return uikMembers;
+	}
+
+	public void setUikMembers(List<UikMember> uikMembers) {
+		this.uikMembers = uikMembers;
+	}
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getLng() {
+		return lng;
+	}
+
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+
+}
